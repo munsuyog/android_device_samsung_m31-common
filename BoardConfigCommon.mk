ee4591275b2b875299b894deb25381fa16d8f86d
Suyog <mundhe.suyog313@gmail.com>
2022-08-14 11:58:27 +0530

    init : userdata partition as f2fs

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -154,7 +154,7 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 ## SELinux
 BOARD_SEPOLICY_TEE_FLAVOR := teegris
 include device/lineage/sepolicy/exynos/sepolicy.mk
-include device/samsung_slsi/sepolicy/sepolicy.mk
+include device/samsung_slsi/sepolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
